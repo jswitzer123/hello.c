ca0b56bc9fd2a726cd3836ff2d8a491c19ac1613
Jared Switzer <jswitzer4@unll.edu>
2019-08-29 14:17:42 -0500

    Create hello copy.c

---
+++ hello copy.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jared Switzer and Jacob Carter
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jared Switzer\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
